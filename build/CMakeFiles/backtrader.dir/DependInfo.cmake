ce5e4c2c871ddb76898fb199ca442951e3dc8ae1
YangYining <YangYN@sjtu.edu.cn>
2021-03-16 11:34:24 +0800

    commit on 'master'

---
+++ build/CMakeFiles/backtrader.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yangyining/Documents/IE/quant/backtraderCpp/src/main.cpp" "/Users/yangyining/Documents/IE/quant/backtraderCpp/build/CMakeFiles/backtrader.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/boost/1.75.0_1/include"
+  "/usr/local/Cellar/ta-lib/0.4.0/include/ta-lib"
+  "/usr/local/Cellar/curl/7.75.0/include/curl"
+  "../lib/CTPForLinux64"
+  "../lib/openssl-1.1.1a/libForLinux64/include"
+  "../lib/log4cplus/include"
+  "../thirdparty/DisruptorCpp"
+  "../thirdparty/fast-cpp-csv-parser"
+  "../thirdparty/cpr/include"
+  "../thirdparty/rapidjson/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/yangyining/Documents/IE/quant/backtraderCpp/build/CMakeFiles/cpr.dir/DependInfo.cmake"
+  "/Users/yangyining/Documents/IE/quant/backtraderCpp/build/CMakeFiles/Disruptor.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
